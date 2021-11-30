e294d61c09492dfca89fb808aef96bd9f41a24a2
Zidan <zidan@Zidans-MacBook-Pro.local>
2021-11-30 18:07:35 -0500

    started fetching from reddit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [userQuery, setUserQuery] = useState('');
+
+  const searchUrl = `https://www.reddit.com/search.json?q=${userQuery}&limit=10&sort=relevance`;
+
+  const handleUserInput = (e: any) => {
+    e.preventDefault();
+    setUserQuery(e.target.value)
+    fetch(`${searchUrl}`).then(response => response.json())
+    .then(data => console.log(data))
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div id="appContainer">      
+      <h1>
+        hello real review
+      </h1>
+      <form action="" onSubmit={e => handleUserInput(e)}>
+        <input type='text' onChange={e => setUserQuery(e.target.value)}/>
+        <input type='submit' value='search reddit' />
+      </form>
+      <div id="results"></div>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
