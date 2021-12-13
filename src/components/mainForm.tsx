fa9917295f4862e92cde7d0211ce5601e6d0d4a4
Karate Kid <shawn.santiago@littlerocket.com>
2021-12-13 09:13:26 -0500

    not broken !

--- src/components/mainForm.tsx
+++ src/components/mainForm.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { sortData,processData } from '../utils';
+interface MyProps {}
 
-class MainForm extends React.Component {
+interface MyState {
+    userQuery:string,
+    setproductData:any
+}
+class MainForm extends React.Component<MyProps, MyState> {
     constructor(props:any) {
         super(props);
         this.state = {
@@ -13,7 +18,7 @@ class MainForm extends React.Component {
         e.preventDefault();
         const searchUrl = `https://www.reddit.com/search.json?q=${this.state.userQuery}%20review&limit=100&sort=relevance`;
         fetch(`${searchUrl}`).then(response => response.json())
-        .then(data => this.setState({setproductData : sortData(processData(data),this.state.userQuery)} )
+        .then(data => this.setState({setproductData : sortData(processData(data),this.state.userQuery)}))
       }
     render() {
         return    <form action="" onSubmit={e => this.handleUserInput(e)}>
